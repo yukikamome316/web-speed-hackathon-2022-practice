f207002737638370b0430a1715b2970ed9e8ba3a
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-02-26 21:27:59 +0900

    fix: development modeの解除

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,9 @@ const DIST_PUBLIC = abs("./dist/public");
 /** @type {Array<import('webpack').Configuration>} */
 module.exports = [
   {
-    devtool: "inline-source-map",
+    // devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "client/index.jsx"),
-    mode: "development",
+    // mode: "development",
     module: {
       rules: [
         {
